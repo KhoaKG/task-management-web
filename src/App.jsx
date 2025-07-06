0156584628167e9064288fb2dd4bf7502ad283f9
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-07-06 19:09:14 +0700

    theme_dark_light

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import Typography from '@mui/material/Typography';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr/>
       <Typography variant='body2' color="text.secondary">
         Test Typography
       </Typography>
--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,27 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#556cd6',
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: teal[500],
+          secondary: deepOrange[500]
+        },
+      },
     },
-    secondary: {
-      main: '#19857b',
+    dark: {
+      palette: {
+        primary: {
+          main: cyan[500],
+          secondary: orange[500]
+        },
+      },
     },
-    error: {
-      main: red.A400,
-    },
-    text:{
-        secondary: red[500],
-    }
   },
-})
+  // ...other properties
+});
 
 export default theme
