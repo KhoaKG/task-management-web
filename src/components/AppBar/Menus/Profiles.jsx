7b2417c0f1bc2caf7a54d18f075838ec2d31fb88
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-07-08 00:22:44 +0700

    board_bar

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -35,7 +35,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar 
-            sx={{ width: 32, height: 32 }} 
+            sx={{ width: 34, height: 34 }} 
             alt='NhatKhoa' 
             src='https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/484599237_2052449475257510_5870588787411688094_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=n9ZDdVZzvMgQ7kNvwF4CaMg&_nc_oc=AdlAf7jVqUb-EkI1tPSA-FSY3SuJVAfvJaxmUmDQVV55IGCXnuqqps3pdkHo908pNyg&_nc_zt=23&_nc_ht=scontent.fsgn5-8.fna&_nc_gid=p2lX0WJvD7Of9A_1ASxMWQ&oh=00_AfSdlcQjjHKihziaQvJ2gCY6s1bDV3l2cCQKykTEuGHOAw&oe=68716229'/>
         </IconButton>
