bb5e5f5c484a178b6702292218c3e1f65d4f4b92
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-07-07 21:24:17 +0700

    app_bar_p2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -28,10 +28,10 @@ function AppBar() {
         backgroundColor: 'white'
       }}>
         <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
-          <AppsIcon />
+          <AppsIcon sx={{color: 'primary.main'}}/>
 
           <Box sx={{display: 'flex', alignItems: 'center', gap: 0.5}}>
-            <SvgIcon component={trelloLogo} inheritViewBox />
+            <SvgIcon sx={{color: 'primary.main'}} fontSize='small' component={trelloLogo} inheritViewBox />
             <Typography variant='span' sx={{fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main'}}>
               Trello
             </Typography>
@@ -46,15 +46,19 @@ function AppBar() {
 
         <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
           <TextField id="outlined-search" label="Search ..." type="search" size='small' />
+          
           <ModeSelect />
+          
           <Tooltip title="Notification">
             <Badge color="secondary" variant="dot" sx={{cursor: 'pointer'}}>
-              <NotificationsNoneIcon />
+              <NotificationsNoneIcon sx={{color: 'primary.main'}}/>
             </Badge>
           </Tooltip>
+          
           <Tooltip title="Help">
-            <HelpOutlineIcon sx={{cursor: 'pointer'}}/>
+            <HelpOutlineIcon sx={{cursor: 'pointer', color: 'primary.main'}}/>
           </Tooltip>
+          
           <Profiles/>
         </Box>  
     </Box>
