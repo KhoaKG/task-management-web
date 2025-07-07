ad4de38f9fb94f24cc7375e2af9dc4ce543a3bfb
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-07-07 12:51:52 +0700

    basic_layout

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,19 @@
 import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
-import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+import { cyan, deepOrange, grey, orange, red, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello:{
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: {
-          main: teal[500],
-          secondary: deepOrange[500]
+          main: grey[100],
+          secondary: deepOrange[500],
+          light: red[500]
         },
       },
     },
