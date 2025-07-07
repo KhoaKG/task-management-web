9e052755b701e82724fcc2d31ce157062e0b8207
Nguyễn Nhật Khoa <KhoaKG@users.noreply.github.com>
2025-07-07 15:55:23 +0700

    import

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,5 +1,5 @@
 
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
 import Box from '@mui/material/Box';
 function AppBar() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-
-import App from './App.jsx'
-import theme from './theme';
+import App from '~/App.jsx'
+import theme from '~/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
